37130bdbf27aa65fdab8052a44667d52ccfa9c9e
Paul Livingstone <paul.livingstone@aquarium-software.com>
2019-12-24 09:30:03 +0000

    Basic JS lessons 31-60 and restructuring

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/word-blanks.js
@@ -0,0 +1,6 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "The " + myNoun + " was very " + myAdjective + " and " + myVerb + " very " + myAdverb +"."; // Only change this line;
