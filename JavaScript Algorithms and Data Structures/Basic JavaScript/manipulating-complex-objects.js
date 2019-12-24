5ed1cf1580c4acdd99872fd64ae0f0a11582d433
Paul Livingstone <paul.livingstone@aquarium-software.com>
2019-12-24 11:42:58 +0000

    Basic JS lessons 80 - 92

---
+++ JavaScript Algorithms and Data Structures/Basic JavaScript/manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Queen",
+    "title": "A Night at the Opera",
+    "release_year": 1975,
+    "formats": [
+      "CD",
+      "Cassette",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+];
