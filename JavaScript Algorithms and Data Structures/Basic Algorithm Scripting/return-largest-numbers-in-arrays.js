26e100c14d125882cace7e8ccbf463561707f7b7
TheFlyer1983 <theflyer1983@gmail.com>
2020-02-28 22:14:35 +0000

    Basic Algorithm Scripting Tasks 3-5

---
+++ JavaScript Algorithms and Data Structures/Basic Algorithm Scripting/return-largest-numbers-in-arrays.js
@@ -0,0 +1,14 @@
+function largestOfFour(arr) {
+  // You can do this!
+  const newArr = [];
+
+  arr.map(i => {
+    newArr.push(i.reduce((x, y) => {
+      return Math.max(x,y);
+    }));
+  });
+  console.log(newArr);
+  return newArr
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
