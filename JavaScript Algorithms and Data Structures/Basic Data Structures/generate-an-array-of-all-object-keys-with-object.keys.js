336c7f975a6e39c835a4ae6808b59c2b786f5880
paullivingstoneAQ <paul.livingstone@aquarium-software.com>
2020-02-28 08:59:11 +0000

    Basic Data Structres final lessons.

---
+++ JavaScript Algorithms and Data Structures/Basic Data Structures/generate-an-array-of-all-object-keys-with-object.keys.js
@@ -0,0 +1,27 @@
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // change code below this line
+  const array = Object.keys(obj);
+  return array;
+  // change code above this line
+}
+
+console.log(getArrayOfUsers(users));
